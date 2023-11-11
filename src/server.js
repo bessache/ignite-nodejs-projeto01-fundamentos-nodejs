298040ea4a87e755e1c6dd4888607883834a2d6d
bessache <bessache@outlook.com>
2023-11-11 20:43:05 -0300

    primeiro comite 03 Corpo requisição JSON

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import http from 'node:http'
+
+const users = []
+const server = http.createServer(async(req, res)=>{
+  const {method, url}= req
+  const buffers =[]
+  for await (const chunck of req) {
+    buffers.push(chunck)
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body= null
+  }
+  if (method == 'GET' && url=='/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+  if (method == 'POST' && url=='/users') {
+    const {name, email} = req.body
+    users.push({
+      id: 1,
+      name,
+      email,
+    })
+    return res.writeHead(201).end()
+  }
+  return res.end('hello ignite')
+})
+server.listen(3333)
+//localhost:3333
---
+++ streams/fundamentals.js
@@ -0,0 +1,32 @@
+import {Readable, Writable, Transform} from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+  _read (){
+    const i = this.index++
+    setTimeout(()=>{
+      if (i> 100){
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf)
+      }
+    }, 1800)
+
+  }
+}
+class MultiplayByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString())*10) 
+    callback()
+  }
+}
+class InverseNumberStream extends Transform {
+  _transform (chunck, encoding, callback) {
+    const transformed = Number(chunck.toString()) * -1
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplayByTenStream())
