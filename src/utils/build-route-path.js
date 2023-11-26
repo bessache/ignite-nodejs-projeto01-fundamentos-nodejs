3366a0c22a78906bbd479db8680bee8086e38b30
bessache <bessache@outlook.com>
2023-11-26 09:56:59 -0300

    commit 05 Rotas com Parametro regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,4 +1,6 @@
 export function buildRoutePath(path) {
   const routeParameterRegex =  /:([a-zA-Z]+)/g
-  console.log(Array.from(path.matchAll(routeParameterRegex)))
+  const pathWithParams = path.replaceAll(routeParameterRegex, '(?<$1>[a-z)-9\-_]+)')
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+  return pathRegex
 }
